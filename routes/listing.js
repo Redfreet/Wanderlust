2e0102c791297f8772e7d81abb713f7de0fe7f6a
Ravi <ravigood7@gmail.com>
2025-04-19 20:10:24 +0530

    all codes

---
+++ routes/listing.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/warpAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+const listingController = require("../controllers/listing.js");
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  // .post(
+  //   isLoggedIn,
+  //   validateListing,
+  //   wrapAsync(listingController.createListing)
+  // );
+  .post(upload.single("listing[image]"), (req, res) => {
+    res.send(req.file);
+  });
+
+//New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+//Index Route
+// router.get("/", wrapAsync(listingController.index));
+
+//Show Route
+// router.get("/:id", wrapAsync(listingController.showListing));
+
+//Edit Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm)
+);
+
+//Create Route
+// router.post(
+//   "/",
+//   isLoggedIn,
+//   validateListing,
+//   wrapAsync(listingController.createListing)
+// );
+
+//Update Route
+// router.put(
+//   "/:id",
+//   isLoggedIn,
+//   isOwner,
+//   validateListing,
+//   wrapAsync(listingController.updateListing)
+// );
+
+//Delete ROute
+// router.delete(
+//   "/:id",
+//   isLoggedIn,
+//   isOwner,
+//   wrapAsync(listingController.destroyListing)
+// );
+
+module.exports = router;
